6e3bd0fe409bfca4565833a18032a5aea9b20d52
soldieritz <136176952+soldieritz@users.noreply.github.com>
2023-06-13 22:32:51 -0500

    Update passport.js

--- server/passport.js
+++ server/passport.js
@@ -1,32 +1,42 @@
-const passport = require("passport");
-const { Strategy } = require("passport-discord");
+const passport = require('passport')
+const { Strategy } = require('passport-discord')
+const DiscordOauth2 = require("discord-oauth2");
+const Discord = require('discord.js')
+const { MessageEmbed } = require('discord.js')
+const client = require('../bot.js')
+const oauth = new DiscordOauth2();
 const config = require("../config");
-let clientID = config.client.id;
+let clientId = config.client.id;
 let clientSecret = config.client.secret;
 let callbackURL = config.url + "/login";
 let scope = config.client.scope;
+
 passport.serializeUser((user, done) => {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+  done(null, obj)
+})
 
-passport.use(
-  new Strategy(
-    {
-      clientID,
-      clientSecret,
-      callbackURL: callbackURL,
-      scope,
-    },
-    (a, r, profile, cb) => {
-      process.nextTick(() => {
-        return cb(null, profile);
-      });
-    }
-  )
+passport.use(new Strategy({
+  clientID: clientId,
+  clientSecret: clientSecret,
+  callbackURL: callbackURL,
+  scope: ["identify", "guilds.join"],
+}, (accesstoken, refreshtoken, profile, cb) => {
+  oauth.addMember({
+	accessToken: accesstoken,
+	botToken: process.env.token,
+	guildId: config.guild,
+	userId: profile.id,
+
+	deaf: true,
+}).then(console.log)
+  process.nextTick(() => {
+    return cb(null, profile)
+  })
+}))
 );
 
 module.exports = passport;
